09449e1ff4353213e4f67bcb03dd04309b530ebd
KrehlK <krehlkasayan@gmail.com>
2022-02-20 18:32:06 -0500

    lets see if the code from here will be put in

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,39 +1,43 @@
 let slidePosition = 0;
-const slides = document.querySelectorAll('.carousel_item');
+const slides = document.getElementsByClassName('carousel__item');
 const totalSlides = slides.length;
 
-document.querySelector('.carousel_button--next')
-  .addEventListener('click', function () {
+document.
+  getElementById('carousel__button--next')
+  .addEventListener("click", function() {
     moveToNextSlide();
   });
-document.querySelector('.carousel__button--prev')
-  .addEventListener("click", function () {
+document.
+  getElementById('carousel__button--prev')
+  .addEventListener("click", function() {
     moveToPrevSlide();
   });
 
 function updateSlidePosition() {
   for (let slide of slides) {
-    slide.classList.remove('carousel_item--visible');
-    slide.classList.add('carousel_item--hidden');
+    slide.classList.remove('carousel__item--visible');
+    slide.classList.add('carousel__item--hidden');
   }
 
-  slides[slidePosition].classList.add('carousel_item--visible');
+  slides[slidePosition].classList.add('carousel__item--visible');
 }
 
 function moveToNextSlide() {
-  if (slidePosition == totalSlides) {
+  if (slidePosition === totalSlides - 1) {
     slidePosition = 0;
   } else {
     slidePosition++;
   }
+
   updateSlidePosition();
 }
 
 function moveToPrevSlide() {
-  if (slidePosition == 0) {
-    slidePosition = 0;
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
   } else {
     slidePosition--;
   }
+
   updateSlidePosition();
 }
