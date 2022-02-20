735611d32c047d1bc8cc46b2495a0ab74f49f0d4
KrehlK <krehlkasayan@gmail.com>
2022-02-20 09:47:50 -0500

    test

---
+++ client/lab_4/app.js
@@ -0,0 +1,42 @@
+let slidePostion = 0;
+const slides = document.getElementsByClassName('carousel_item');
+const totalSlides = slides.length;
+
+document
+  .getElementById('carousel_button--next')
+  .addEventListener('click', function(){
+    moveToNextSlide();
+  });
+document
+  .getElementById('carousel_button--prev')
+  .addEventListener("click", function () {
+    moveToPrevSlide();
+  });
+
+function updateSlidePosition() {
+  for (let slide of slides) {
+    slide.classList.remove('carousel_item--visible');
+    slide.classList.add('carousel_item--hidden');
+  }
+
+  slides[slidePosition].classList.add('carousel_item--visible');
+}
+
+function moveToNextSlide() {
+  if (slidePostion === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePostion === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+
+  updateSlidePosition();
+}
